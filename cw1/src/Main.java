c574f32ca29877f10f3566499ebb577192f7c732
Jakub224 <jakub3szczesniak@gmail.com>
2025-09-23 11:16:56 +0200

    cos

--- cw1/src/Main.java
+++ cw1/src/Main.java
@@ -2,6 +2,8 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        double stopnieCelsjusza, stopnieFahrenheita;
 
+        system.out.print("Podaj temperature w stopniach Celcjusza: ");
     }
 }
