2382b18492c7cc94f79d9ab22f1103b9554c472c
Jakub224 <jakub3szczesniak@gmail.com>
2025-09-23 11:30:39 +0200

    cos

--- cw1/src/Main.java
+++ cw1/src/Main.java
@@ -1,9 +1,13 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         double stopnieCelsjusza, stopnieFahrenheita;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Podaj temperature w stopniach Celsjusza: ");
+        stopnieCelsjusza = scanner.nextDouble();
 
-        system.out.print("Podaj temperature w stopniach Celcjusza: ");
+        stopnieFahrenheita = 1.8 * stopnieCelsjusza + 32;
+
+        System.out.println("Temperatura w stopniach Fahrenheita: " + stopnieFahrenheita);
     }
 }
